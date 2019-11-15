7abb2c148bfaf1221680173c0071bb0bd2c14004
natasha <natalevi@gmail.com>
2019-11-15 08:44:50 -0500

    added tests

--- pset_5/tasks.py
+++ pset_5/tasks.py
@@ -13,37 +13,28 @@ from csci_utils.luigi.task import Requires, Requirement, TargetOutput
 class YelpReviews(ExternalTask):
     '''Write an ExternalTask named YelpReviews which uses the appropriate dask target'''
     def output(self):
-        print("Yelp Reviews is called output here")
-        file_path = "s3://ohsunny/pset_5/yelp/"
-        output = CSVTarget(file_path, flag=None, glob= "*.csv")
+     #   file_path = "s3://ohsunny/pset_5/yelp/"
+     #   output = CSVTarget(file_path, flag=None, glob= "*.csv")
 
-        return  output
+        return  CSVTarget("s3://ohsunny/pset_5/yelp/", flag=None, glob= "*.csv")
 
 
 class CleanedReviews(Task):
     subset = BoolParameter(default=True)
     requires = Requires()
     yelp_reviews = Requirement(YelpReviews)
-    print("Cleaned Reviews has been called")
 
     output = TargetOutput( ext="/", target_class=ParquetTarget, glob="*.parquet")
 
     def run(self):
-        print ("run has been called")
 
         numcols = ["funny", "cool", "useful", "stars"]
         dsk = self.input()["yelp_reviews"].read_dask(storage_options=dict(requester_pays=True),
                                                      dtype={col:"float64" for col in numcols},
                                                      parse_dates=['date'])
-        # dsk = self.input()["reviews"].read_dask(storage_options=dict(requester_pays=True), dtype={col:"float64" for col in numcols}, parse_dates=['dates'])
-
-        print (dsk.head())
-
         if self.subset:
             dsk = dsk.get_partition(0)
 
-
-        # dsk = dsk.merge(df2.set_index('CPT Code'), on='CPT Code')
         out = (dsk.dropna(subset=["user_id", "date"])[dsk["review_id"].str.len()==22]
             .set_index("review_id")
             .fillna(value={col: 0.0 for col in numcols})
@@ -57,17 +48,16 @@ class CleanedReviews(Task):
 
 class ByDecade(Task):
     subset = BoolParameter(default=True)
-   # requires=Requires()
     decade_reviews=Requirement(CleanedReviews)
-    print("before output function")
+
     output = TargetOutput( ext="/by-decade/", target_class=ParquetTarget, glob="*.parquet")
-    print("output is", output)
+
 
     def requires(self):
         return CleanedReviews(subset=self.subset)
 
     def run(self):
-        print("got to run")
+
         dsk = self.input().read_dask(
             columns=["date", "length_reviews"],
             parse_dates=['date'])\
@@ -84,21 +74,18 @@ class ByDecade(Task):
                )
 
 
-        self.output().write_dask(out, compression='gzip')
-        self.print_results()
-        print("the above should be results by decade", out)
+      #  self.output().write_dask(out, compression='gzip')
+        ByStars.print_results(self, out)
 
-    def print_results(self):
-        print("visited print results")
-        print(self.output().read_dask().compute())
+
+ #   def print_results(self):
+  #      print(self.output().read_dask().compute())
 
 class ByStars(Task):
     subset = BoolParameter(default=True)
     requires=Requires()
     cleaned_review=Requirement(CleanedReviews)
     output = TargetOutput( ext="/by-stars/", target_class=ParquetTarget, glob="*.parquet")
-    #output = TargetOutput(file_pattern=os.path.abspath("./data/by-stars"), target_class=ParquetTarget, ext="")
-    #output = TargetOutput( ext='files.parquet/', target_class=ParquetTarget, glob="*.parquet")
 
     def run(self):
         dsk = self.input()["cleaned_review"].read_dask(columns=["length_reviews", "stars"])
@@ -113,10 +100,11 @@ class ByStars(Task):
             .astype({"length_reviews":"int32"})
         )
 
-        self.output().write_dask(out, compression='gzip')
-        self.print_results()
+       # self.output().write_dask(out, compression='gzip')
+        self.print_results(out)
 
-    def print_results(self):
+    def print_results(self, out):
+        self.output().write_dask(out, compression='gzip')
         print(self.output().read_dask().compute())
 
 
--- pset_5/tests/test_pset_5.py
+++ pset_5/tests/test_pset_5.py
@@ -1,7 +1,84 @@
 from unittest import TestCase
-from luigi import Task, Parameter
-
+from luigi import Task, Parameter, ExternalTask, LocalTarget, LuigiStatusCode, build
+from pset_5.tasks import YelpReviews, ByDecade, ByStars, BoolParameter, CleanedReviews
+from tempfile import TemporaryDirectory
+from luigi.contrib.external_program import ExternalProgramTask
+from luigi.task_register import load_task
+import os
 
 class HashTests(TestCase):
     def test_data(self):
         self.assertEqual(5, 5)
+
+
+    def test_external_tasks(self):
+        task = CleanedReviews()
+        task2 = ByStars()
+        task3 = ByDecade()
+        task4 =YelpReviews()
+        assert isinstance(task, Task)
+        assert isinstance(task4, ExternalTask)
+        assert isinstance(task3, Task)
+        assert isinstance(task2, Task)
+
+    '''def test_parameters(self):
+        task = CleanedReviews()
+        task2 = ByStars()
+        task3 = ByDecade()
+        task4 =YelpReviews()
+        assert isinstance(task.subset, BoolParameter())
+     '''
+
+class TaskTest(TestCase):
+    """testing Stylize is a External Program task"""
+
+
+    def test_CleanedReviews(self):
+        self.assertEqual(
+            build(
+                [CleanedReviews()],
+                local_scheduler=True,
+                detailed_summary=True,
+            ).status,
+            LuigiStatusCode.SUCCESS,
+        )
+
+    def test_ByStars(self):
+        self.assertEqual(
+            build(
+                [ByStars()],
+                local_scheduler=True,
+            ).status,
+            LuigiStatusCode.SUCCESS,
+        )
+
+    def test_ByDecade(self):
+        self.assertEqual(
+            build(
+                [ByDecade()],
+                local_scheduler=True,
+                detailed_summary=True,
+            ).status,
+            LuigiStatusCode.SUCCESS,
+        )
+
+    def test_ByStars(self):
+        self.assertEqual(
+            build(
+                [ByStars()],
+                local_scheduler=True,
+                detailed_summary=True,
+            ).status,
+            LuigiStatusCode.SUCCESS,
+        )
+class yelp_YelpTest(YelpReviews):
+    IMAGE_ROOT = Parameter()
+
+    def input(self):
+        return LocalTarget(os.path.join(self.IMAGE_ROOT, "test.jpg"), format=format.Nop)
+
+    def requires(self):
+        pass
+
+
+
